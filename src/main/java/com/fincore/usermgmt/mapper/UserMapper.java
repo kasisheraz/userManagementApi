a1b8409343091c982cf1ca5f97f3f1d0b492d1a1
kasisheraz <kasisheraz@users.noreply.github.com>
2025-12-12 20:44:38 +0000

    MapStruct: ignore null properties during update mapping (UserMapper)

---
+++ src/main/java/com/fincore/usermgmt/mapper/UserMapper.java
@@ -0,0 +1,42 @@
+package com.fincore.usermgmt.mapper;
+
+import com.fincore.usermgmt.dto.UserCreateDTO;
+import com.fincore.usermgmt.dto.UserDTO;
+import com.fincore.usermgmt.dto.UserUpdateDTO;
+import com.fincore.usermgmt.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "role", source = "role.name")
+    UserDTO toUserDTO(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "failedLoginAttempts", ignore = true)
+    @Mapping(target = "lockedUntil", ignore = true)
+    @Mapping(target = "lastLoginAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User toUser(UserCreateDTO userCreateDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "failedLoginAttempts", ignore = true)
+    @Mapping(target = "lockedUntil", ignore = true)
+    @Mapping(target = "lastLoginAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDto(UserUpdateDTO userUpdateDTO, @MappingTarget User user);
+}
